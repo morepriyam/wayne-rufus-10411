d85ad55e9186bca1c575aa4c04f8ef8a845b5c6c
Benjamin True Mumma <benmumma73@gmail.com>
2026-03-16 16:24:56 -0400

    Update RPM of Auto-Shoot to 3750.

--- src/main/java/frc/robot/commands/PrepareShotCommand.java
+++ src/main/java/frc/robot/commands/PrepareShotCommand.java
@@ -34,9 +34,9 @@ public class PrepareShotCommand extends Command {
     );
 
     static {
-        distanceToShotMap.put(Inches.of(52.0), new Shot(5000, 0.19));
-        distanceToShotMap.put(Inches.of(114.4), new Shot(5000, 0.40));
-        distanceToShotMap.put(Inches.of(165.5), new Shot(5000, 0.48));
+        distanceToShotMap.put(Inches.of(52.0), new Shot(3750, 0.19));
+        distanceToShotMap.put(Inches.of(114.4), new Shot(3750, 0.40));
+        distanceToShotMap.put(Inches.of(165.5), new Shot(3750, 0.48));
     }
 
     private final Shooter shooter;
